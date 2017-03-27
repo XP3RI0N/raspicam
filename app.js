9f7d381c7d675486f4f1973276298a775272afca
Sebaymax <vulpotlood@outlook.com>
2017-03-27 11:58:01 +0200

    Added more messages to LCD

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ lcd.on('ready', function() {
 	setInterval(function() {
 		lcd.setCursor(0, 0);
 		lcd.print(new Date().toString().substring(16, 24));
+		lcd.setCursor(0, 1);
+		lcd.print("Push the button");
 	}, 1000);
 });
 
