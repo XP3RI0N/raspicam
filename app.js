f7175c67d68c78a11525ffb64463c52d9eb64f80
Stijn Vandendriessche <vandendriessche.stijn@telenet.be>
2017-04-24 11:32:29 +0200

    probeersel met express en socket.io

--- app.js
+++ app.js
@@ -89,3 +89,21 @@ function moveCamToPreset(presetID) {
 		}
 	})
 }
+
+
+
+// webserver
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(80);
+
+app.use(express.static('webinterface'));
+
+io.on('connection', function (socket) {
+	socket.emit('news', { hello: 'world' });
+	socket.on('my other event', function (data) {
+		console.log(data);
+	});
+});
