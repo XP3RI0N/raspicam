c6247a539703e4bbe0b6d3fbba3f6538ddb01701
Stijn Vandendriessche <vandendriessche.stijn@telenet.be>
2017-04-24 10:36:11 +0200

    added request to move cam

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
 // var express = require('express');
 
-var gpio = require('gpio');
+var gpio  = require('gpio');
 var gpio5 = gpio.export(5, {
 	direction: "in",
-	ready: function() {
+	ready    : function () {
 		console.log("gpio 5 ready")
 	}
 });
 
-gpio5.on("change", function(val) {
+gpio5.on("change", function (val) {
 	// value will report either 1 or 0 (number) when the value changes
 	console.log(val)
+	if (val === 1) {
+		moveCam();
+	}
 });
 
 var Lcd = require('lcd'),
 	lcd = new Lcd({
-		rs: 20,
-		e: 16,
+		rs  : 20,
+		e   : 16,
 		data: [6, 13, 19, 26],
 		cols: 16,
 		rows: 2
 	});
 
-lcd.on('ready', function() {
+lcd.on('ready', function () {
 	console.log("lcd ready");
-	setInterval(function() {
+	setInterval(function () {
 		lcd.setCursor(0, 0);
 		lcd.print(new Date().toString().substring(16, 24));
-		lcd.once("printed", function() {
+		lcd.once("printed", function () {
 			lcd.setCursor(0, 1);
 			lcd.print("Push the button");
 		})
@@ -35,14 +38,32 @@ lcd.on('ready', function() {
 });
 
 // If ctrl+c is hit, free resources and exit.
-process.on('SIGINT', function() {
+process.on('SIGINT', function () {
 	lcd.clear(
 		function () {
 			lcd.close(function () {
 				process.exit();
 			});
 		}
+	);
 
 	//lcd.close();
 	//process.exit();
 });
+
+
+function moveCam() {
+	var request = require('request');
+
+	request("http://172.23.49.1/axis-cgi/com/ptz.cgi?camera=1&pan=10", {
+		'auth': {
+			'user'           : 'student',
+			'pass'           : 'vandeweek',
+			'sendImmediately': false
+		}
+	}, function (error, response, body) {
+		if (body) {
+			console.log("Fout: " + body);
+		}
+	})
+};
