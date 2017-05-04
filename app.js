094e959a3d62d7a9e5ad7c5ef818b9b368145779
Sebaymax <vulpotlood@outlook.com>
2017-05-04 14:33:10 +0200

    DING DONG again

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var gpio    = require('gpio');
-var Lcd     = require('lcd');
+var gpio = require('gpio');
+var Lcd = require('lcd');
 
 
 // webserver
 var request = require('request');
 var express = require('express');
-var app     = express();
-var server  = require('http').Server(app);
-var io      = require('socket.io')(server);
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 server.listen(8080);
 
@@ -16,111 +16,115 @@ app.use(express.static('webinterface'));
 io.on('connection', function (socket) {
 
 
-	socket.on('command', function (msg) {
+    socket.on('command', function (msg) {
 
-		console.log(msg);
+        console.log(msg);
 
-		if (msg.command === "move") {
-			moveCam(msg.param);
-		} else if (msg.command === "preset") {
-			moveCamToPreset(msg.param);
-		}
-	})
+        if (msg.command === "move") {
+            moveCam(msg.param);
+        } else if (msg.command === "preset") {
+            moveCamToPreset(msg.param);
+        }
+    })
 });
 
 //<editor-fold desc="pi hardware">
 // GPIO
 var gpio5 = gpio.export(5, {
-	direction: "in",
-	ready    : function () {
-		console.log("GPIO 5 - ready")
-	}
+    direction: "in",
+    ready: function () {
+        console.log("GPIO 5 - ready")
+    }
 });
 
 gpio5.on("change", function (val) {
-	// value will report either 1 or 0 (number) when the value changes
-	console.log(val);
-	if (val === 1) {
-		moveCam("home");
-		console.log("DING DONG!");
-	}
+    // value will report either 1 or 0 (number) when the value changes
+    // console.log(val);
+    if (val === 1) {
+        moveCam("home");
+        console.log("... DONG!");
+    } else {
+        console.log("DING ...")
+    }
 });
 
 // LCD
 lcd = new Lcd({
-	rs  : 20,
-	e   : 16,
-	data: [6, 13, 19, 26],
-	cols: 16,
-	rows: 2
+    rs: 20,
+    e: 16,
+    data: [6, 13, 19, 26],
+    cols: 16,
+    rows: 2
 });
 
 lcd.on('ready', function () {
-	var monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "Septembr", "October", "November", "December"
-];
+    var monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "Septembr", "October", "November", "December"
+    ];
 
-	console.log("LCD - ready");
-	setInterval(function () {
-		var d = new Date();
-		lcd.setCursor(0, 0);
-		lcd.print(d.toString().substring(16, 24));
-		lcd.once("printed", function () {
-			lcd.setCursor(0, 1);
-			// lcd.print("Push the button");
-			lcd.print(d.getDate().toString() + " " + monthNames[d.getMonth()] + " " + d.getFullYear().toString());
-		})
-	}, 1000);
+    console.log("LCD - ready");
+    setInterval(function () {
+        var d = new Date();
+        lcd.setCursor(0, 0);
+        lcd.print(d.toString().substring(16, 24));
+        lcd.once("printed", function () {
+            lcd.setCursor(0, 1);
+            // lcd.print("Push the button");
+            lcd.print(d.getDate().toString() + " " + monthNames[d.getMonth()] + " " + d.getFullYear().toString());
+        })
+    }, 1000);
 });
 
 // If ctrl+c is hit, free resources and exit.
 process.on('SIGINT', function () {
-	lcd.clear(
-		function () {
-			lcd.close(function () {
-				process.exit();
-			});
-		}
-	);
+    lcd.clear(
+        function () {
+            lcd.close(function () {
+                process.exit();
+            });
+        }
+    );
 });
 //</editor-fold>
 
 function moveCam(move) {
-	if (!move)
-		return;
+    if (!move)
+        return;
 
-	request("http://172.23.49.1/axis-cgi/com/ptz.cgi?camera=1&move=" + move, {
-		'auth': {
-			'user'           : 'student',
-			'pass'           : 'tasjekoffie',
-			'sendImmediately': false
-		}
-	}, function (error, response, body) {
-		if (body) {
-			console.log("Error: " + body);
-		}
-	})
+    move = move.toLowerCase();
+
+    request("http://172.23.49.1/axis-cgi/com/ptz.cgi?camera=1&move=" + move, {
+        'auth': {
+            'user': 'student',
+            'pass': 'tasjekoffie',
+            'sendImmediately': false
+        }
+    }, function (error, response, body) {
+        if (body) {
+            console.log("Error: " + body);
+        }
+    })
 };
 
 function moveCamToPreset(presetID) {
-	if (!presetID)
-		return;
+    if (!presetID)
+        return;
 
-	var presets = require('./webinterface/js/presets.js').list;
-	var preset = presets[presetID - 1];
+    var presets = require('./webinterface/js/presets.js').list;
+    var preset = presets[presetID - 1];
 
-	console.log(preset);
+    console.log(preset);
 
-	request("http://172.23.49.1/axis-cgi/com/ptz.cgi?camera=1&pan=" + preset.pan + "&tilt=" + preset.tilt + "&zoom=" + preset.zoom, {
-		'auth': {
-			'user'           : 'student',
-			'pass'           : 'tasjekoffie',
-			'sendImmediately': false
-		}
-	}, function (error, response, body) {
-		if (body) {
-			console.log("Error: " + body);
-		}
-	})
+    request("http://172.23.49.1/axis-cgi/com/ptz.cgi?camera=1&pan=" + preset.pan + "&tilt=" + preset.tilt + "&zoom=" + preset.zoom, {
+        'auth': {
+            'user': 'student',
+            'pass': 'tasjekoffie',
+            'sendImmediately': false
+        }
+    }, function (error, response, body) {
+        if (body) {
+            console.log("Error: " + body);
+        }
+    })
 
 }
